c2e75435e5430ef456642dd585d3b469abff6c16
Nathan Green <hello@nathgreen.co.uk>
2019-01-31 05:17:27 +0000

    add validator class

---
+++ src/index.js
@@ -0,0 +1,30 @@
+export default class Validator {
+  constructor() {
+    this.rules = [];
+    this.error = [];
+  }
+
+  rule({ fn, params = [], message }) {
+    this.rules.push({
+      fn,
+      params,
+      message
+    });
+    return this;
+  }
+
+  errors() {
+    return this.error;
+  }
+
+  validate(toTest) {
+    this.error = [];
+
+    this.rules.forEach((rule) => {
+      const { fn, params, message } = rule;
+      if (!fn(toTest, ...params)) this.error.push(message);
+    });
+
+    return this.error.length === 0;
+  }
+}
